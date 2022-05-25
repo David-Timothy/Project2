636cc0f899926598739fae47562a541a9b385019
drtimoth <david825@revature.net>
2022-05-25 14:09:58 -0700

    Expanded Shop
    added a json file to control the items available in shops

--- angular-webapp/src/app/shop/shop.component.ts
+++ angular-webapp/src/app/shop/shop.component.ts
@@ -3,6 +3,7 @@ import { player } from '../scriptFiles/actor';
 import {shop} from '../scriptFiles/shop'
 import { Router } from '@angular/router';
 import { PlayerService } from '../services/player.service';
+import * as getAbilities from "../json/abilities.json";
 
 @Component({
   selector: 'app-shop',
@@ -17,8 +18,21 @@ export class ShopComponent extends shop implements OnInit{
   }
 
   ngOnInit(): void {
-    this.addSkill("Evade", "boost-defence", 0, 1, 1, true);
+    var abilities = JSON.parse(JSON.stringify(getAbilities));
 
+    for(const skill of abilities.skills) {
+      this.addSpell(skill.name, skill.effect, skill.sides, skill.cost, skill.coinCost, skill.selfTargeted);
+    }
+    //this.addSkill("Evade", "boost-defence", 0, 1, 1, true);
+
+    for(const spell of abilities.spells) {
+      this.addSkill(spell.name, spell.effect, spell.sides, spell.cost, spell.coinCost, spell.selfTargeted);
+    }
+
+    for(const item of abilities.items) {
+      this.addItem(item.name, item.effect, item.sides, item.stock, item.coinCost, item.selfTargeted);
+    }
+    /*
     this.addSpell("Firebolt", "burn", 8, 2, 2, false);
     this.addSpell("Freeze", "slow", 6, 2, 1, false);
     this.addSpell("Confuse", "daze", 0, 1, 2, false);
@@ -26,6 +40,7 @@ export class ShopComponent extends shop implements OnInit{
     this.addItem("Sword", "none", 6, -1, 4, false);
     this.addItem("Bomb", "none", 10, 0, 4, false);
     this.addItem("Health Potion", "heal", -10, 0, 1, true);
+    */
   }
 
   getCoins(): number {
